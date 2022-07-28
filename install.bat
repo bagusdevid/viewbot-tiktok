bf27494a6bea0980d3c430e66d731228367a3b50
TB Gita Priadi <66976191+bagusdevid@users.noreply.github.com>
2022-07-28 14:43:38 +0700

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,11 @@
+@echo off
+color a 
+
+echo INSTALLING...
+
+pip install requests bs4 cursor pystyle pillow
+
+echo
+echo
+
+pause
---
+++ running.bat
@@ -0,0 +1,11 @@
+@echo off
+color a 
+
+echo Running TIkTok View by bagusdevid - TB GITA PRIADI..
+
+python viewbot.py
+
+echo
+echo
+
+pause
